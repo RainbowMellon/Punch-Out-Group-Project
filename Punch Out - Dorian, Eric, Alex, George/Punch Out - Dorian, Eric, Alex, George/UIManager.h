8524de214cccbe669d2c292d3ae79becc42fab11
RanbowMellon <109363895+RanbowMellon@users.noreply.github.com>
2023-02-13 15:11:10 -0600

    Update UIManager.h

--- Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/UIManager.h
+++ Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/UIManager.h
@@ -1,5 +1,20 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+
+
 class UIManager
 {
+public:
+  UIManager();
+  ~UIManager();
+  void winScreen(float);
+  void loseScreen();
+  void drawText(string);
+  //give how much to take away
+  void updatePlayerBar(int);
+  void updateOppoBar(int);
+
+private:
+  
 };
 
