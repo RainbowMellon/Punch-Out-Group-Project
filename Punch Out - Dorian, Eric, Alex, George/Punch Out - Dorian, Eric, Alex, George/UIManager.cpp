238186af6fa7fb2638369d8c3bbfb6ea8f30c7db
RanbowMellon <109363895+RanbowMellon@users.noreply.github.com>
2023-02-13 15:29:41 -0600

    Update UIManager.cpp

--- Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/UIManager.cpp
+++ Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/UIManager.cpp
@@ -1,1 +1,45 @@
 #include "UIManager.h"
+#include <SFML/Graphics.hpp>
+
+
+UIManager::UIManager(&sf::RenderWindow newWindow, &sf::View newView)
+{
+  window = newWindow;
+  view = newView;
+}
+
+
+UIManager::~UIManager()
+{
+  
+}
+
+
+void UIManager::winScreen(sf::float time)
+{
+  
+}
+
+
+void UIManager::loseScreen()
+{
+  
+}
+
+
+void UIManager::drawText(std::string text)
+{
+  
+}
+
+
+void UIManager::updatePlayerHealth(int damage)
+{
+  
+}
+
+
+void UIManager::updateOppoHealth(int damage)
+{
+  
+}
