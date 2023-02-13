df01872408bb7e891ff3da1128d9378d7e101c8a
RanbowMellon <109363895+RanbowMellon@users.noreply.github.com>
2023-02-13 15:54:35 -0600

    Update Punch Out - Dorian, Eric, Alex, George.cpp

---
+++ Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George.cpp
@@ -0,0 +1,26 @@
+#include <SFML/Graphics.hpp>
+#include "Game.h"
+#include <iostream>
+
+
+int main()
+{
+  sf::RenderWindow window(sf::VideoMode(1536, 960), "Punch-Out");
+  sf::View view(sf::FloatRect(0, 0, 512, 480));
+  
+  while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+    
+        // window.draw(...);
+
+        window.display();
+    }
+}
