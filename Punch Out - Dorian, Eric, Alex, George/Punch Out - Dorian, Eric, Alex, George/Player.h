d69ac10a4094ead89befe8dbf7016dcb6eb28d5f
AlexElletson1 <125312839+AlexElletson1@users.noreply.github.com>
2023-02-15 15:03:38 -0600

    Update Player.h

--- Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/Player.h
+++ Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/Player.h
@@ -1,5 +1,24 @@
 #pragma once
+#include"Opponent.h"
+
 class Player
 {
-};
+public:
+	Player();
+	~Player();
 
+	void draw();
+	void update();
+	//health is the heart points that make you wended
+	int getHealth();
+	//damage is your hp bar that determines when you get ko'd
+	int getDamage();
+	int getStarCount();
+	void wasHit(Opponent&);
+
+
+private:
+	//moveCD is move cooldown
+	int health, damage, starCount, moveCD;
+	bool isWinded;
+};
