3a009f754dadeaba75d4a883f08042a3b3198643
RanbowMellon <109363895+RanbowMellon@users.noreply.github.com>
2023-02-15 14:58:20 -0600

    Update UIManager.cpp

--- Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/UIManager.cpp
+++ Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/UIManager.cpp
@@ -1,7 +1,9 @@
 #include "UIManager.h"
+#include "Game.h"
 #include <SFML/Graphics.hpp>
 
 
+//pass a reference to the window and view for more convinent drawing
 UIManager::UIManager(&sf::RenderWindow newWindow, &sf::View newView)
 {
   window = newWindow;
@@ -15,6 +17,7 @@ UIManager::~UIManager()
 }
 
 
+//give the time on the win clock
 void UIManager::winScreen(sf::float time)
 {
   
@@ -23,23 +26,38 @@ void UIManager::winScreen(sf::float time)
 
 void UIManager::loseScreen()
 {
-  
+  (*window).draw();
 }
 
-
+//give the text you want to draw
 void UIManager::drawText(std::string text)
 {
   
 }
 
 
+//give how much damage the player takes
 void UIManager::updatePlayerHealth(int damage)
 {
   
 }
 
 
+//give how much damage the player takes
 void UIManager::updateOppoHealth(int damage)
+{
+  
+}
+
+
+//pass a refrence to the player and opponent to access the states
+void UIManager::drawStats(&Player player, &Opponent opponent)
+{
+  
+}
+
+
+void roundStartScreen(&Player&, Opponent)
 {
   
 }
