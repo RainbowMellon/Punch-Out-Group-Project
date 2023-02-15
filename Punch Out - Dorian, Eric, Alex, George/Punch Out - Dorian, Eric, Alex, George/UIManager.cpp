5dba47b3e7c2b34f939e810067beb2de60364759
RanbowMellon <109363895+RanbowMellon@users.noreply.github.com>
2023-02-15 15:37:59 -0600

    UI managing (#1)
    * Update UIManager.h
    
    * Update UIManager.cpp
    
    * Update UIManager.h
    
    * Update Punch Out - Dorian, Eric, Alex, George.cpp
    
    * Update UIManager.cpp
    
    * Update UIManager.h
    
    * Update UIManager.cpp
    
    * Update UIManager.cpp
    
    * Update UIManager.cpp
    
    * Update UIManager.h

--- Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/UIManager.cpp
+++ Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/UIManager.cpp
@@ -1,1 +1,60 @@
 #include "UIManager.h"
+#include "Game.h"
+#include <string>
+#include <SFML/Graphics.hpp>
+
+
+//pass a reference to the window and view for more convinent drawing
+//pass a reference to the player to get access stats
+UIManager::UIManager(&sf::RenderWindow newWindow, &sf::View newView, &Player newPlayer)
+{
+  window = newWindow;
+  view = newView;
+  player = newPlayer;
+}
+
+
+UIManager::~UIManager()
+{
+  
+}
+
+
+//give the time on the win clock
+void UIManager::winScreen(sf::float time)
+{
+  //view.setCenter(0, 0);
+  //coordinate of the win screen
+  //view.move(?, ?);
+  
+  //how long it took to win
+  //window.draw(sf::Text(std::to_string(time)));
+}
+
+
+void UIManager::loseScreen()
+{
+  
+}
+
+//give the text you want to draw
+void UIManager::drawText(std::string text)
+{
+  
+}
+
+
+//pass a refrence to the opponent to access the states
+//used for while fighting
+void UIManager::drawStats(&Opponent opponent)
+{
+  
+}
+
+
+//pass a refrence to the opponent to access the states
+//used for inbetween fighting
+void roundStartScreen(&Opponent opponent)
+{
+  
+}
--- Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/UIManager.h
+++ Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/UIManager.h
@@ -1,5 +1,23 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include <string>
+#include "Game.h"
+
+
 class UIManager
 {
+public:
+  UIManager(&sf::RenderWindow, &sf::View, &Player);
+  ~UIManager();
+  void winScreen(float);
+  void loseScreen();
+  void drawText(std::string);
+  void drawStats(&Player, &Opponent);
+  void roundStartScreen(&Player&, Opponent);
+
+private:
+  *sf::RenderWindow window;
+  *sf::View view;
+  Player player;
 };
 
