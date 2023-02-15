d069be3d44c12908948ce0a4cd59a3390aeff6f6
RanbowMellon <109363895+RanbowMellon@users.noreply.github.com>
2023-02-15 15:34:10 -0600

    Update UIManager.h

--- Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/UIManager.h
+++ Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/UIManager.h
@@ -7,18 +7,17 @@
 class UIManager
 {
 public:
-  UIManager(&sf::RenderWindow, &sf::View);
+  UIManager(&sf::RenderWindow, &sf::View, &Player);
   ~UIManager();
   void winScreen(float);
   void loseScreen();
   void drawText(std::string);
-  void updatePlayerHealth(int);
-  void updateOppoHealth(int);
   void drawStats(&Player, &Opponent);
   void roundStartScreen(&Player&, Opponent);
 
 private:
   *sf::RenderWindow window;
   *sf::View view;
+  Player player;
 };
 
