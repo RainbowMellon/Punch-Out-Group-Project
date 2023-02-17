25c0c4f7ebe7fc31e94878de4a7427dbe5724969
OddDman123 <125312692+OddDman123@users.noreply.github.com>
2023-02-17 12:56:57 -0600

    Player and Game updated .h and .cpp, added Sound
    What I spent my morning trying to do

--- Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/Player.cpp
+++ Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/Player.cpp
@@ -1,1 +1,67 @@
 #include "Player.h"
+#include <iostream>
+
+Player::Player()
+{
+	isWinded = false;
+	keyPressed = false;
+	moveCoolD = 0;
+	dir = 0;
+	texture.loadFromFile("sprites/littleMac.png"); //FullSpriteSheet
+	sprite.setTexture(texture);
+	sprite.setTextureRect(sf::IntRect(6, 0, 16, 44)); //Just a rectangle covering the sprite sheet
+	sprite.scale(1.5, 1.5);
+	sprite.setOrigin(16 / 2, 44 / 2);
+	sprite.setPosition(256 / 2, 240 * 0.75);
+
+	buffer.loadFromFile("sounds/Dodge.flac");
+	sound.setBuffer(buffer);
+}
+
+void Player::drawPlayer(sf::RenderWindow& window)
+{
+	window.draw(sprite);
+}
+
+void Player::updatePlayer(sf::Event& event)
+{
+
+	if (event.type == sf::Event::KeyReleased) //No repeating keys if key is held down
+		keyPressed = false;
+	
+
+	if ((sf::Keyboard::isKeyPressed(sf::Keyboard::A) && moveCoolD <= 0) && !keyPressed) //Move right
+	{
+		keyPressed = true;
+		moveCoolD = 50;
+		dir = -2;
+		sound.play();
+	}
+	if ((sf::Keyboard::isKeyPressed(sf::Keyboard::D) && moveCoolD <= 0) && !keyPressed) //move left
+	{
+		keyPressed = true;
+		moveCoolD = 50;
+		dir = 2;
+		sound.play();
+	}
+
+
+	if (moveCoolD > 30)
+	{
+		sprite.move(sf::Vector2f(dir, 0));
+		moveCoolD--;
+	}
+	else if (moveCoolD > 20)
+	{
+		sprite.setTextureRect(sf::IntRect(6 + 16 + 16, 0 + 44, 16, 44)); // when it gets to this point change the rect on the texture
+		moveCoolD--;
+	}
+	else if (moveCoolD > 0)
+	{
+		sprite.setTextureRect(sf::IntRect(6, 0, 16, 44));
+		sprite.move(sf::Vector2f(dir * -1, 0));
+		moveCoolD--;
+	}
+}
+
+Player::~Player(){}
--- Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/Player.h
+++ Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/Player.h
@@ -1,39 +1,21 @@
 #pragma once
-#include"Opponent.h"
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 class Player
 {
 public:
 	Player();
 	~Player();
-
-	void draw();
-	void update();
-	
-	void setHealth(int);
-	void setDamage(int);
-	
-	int getPosition();
-	
-	//health is the heart points that make you wended
-	int getHealth();
-	//damage is your hp bar that determines when you get ko'd
-	int getDamage();
-	int getStarCount();
-	void wasHit(Opponent&);
-
+	void updatePlayer(sf::Event&);
+	void drawPlayer(sf::RenderWindow&);
 
 private:
-	//moveCD is move cooldown
-	int health, damage, starCount, moveCD;
-	/*
-	position is what position mac is in
-	0 = standing still
-	1 = dodging right 
-	2 = dodging back
-	3 = dodging left
-	4 = blocking
-	*/
-	int position;
-	bool isWinded;
+	sf::Sprite sprite;
+	sf::Texture texture;
+	int moveCoolD, dir;
+	bool isWinded, keyPressed;
+	sf::SoundBuffer buffer;
+	sf::Sound sound;
 };
+
