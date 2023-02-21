01f1b6665a75eb8e53152f6c6a746bc43cc879aa
EricSchultz05 <125312833+EricSchultz05@users.noreply.github.com>
2023-02-21 15:59:38 -0600

    Add files via upload

--- Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/GlassJoe.cpp
+++ Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/GlassJoe.cpp
@@ -6,15 +6,21 @@ GlassJoe::GlassJoe()
 	name = "Glass Joe";
 	location = "Paris, France";
 	stats = "1-99      1 KO";
+	texture.loadFromFile("sprites/littleMac.png");
+	sprite.setTexture(texture);
+	sprite.setTextureRect(sf::IntRect(6, 0, 16, 44));
+	sprite.scale(1.5, 1.5);
+	sprite.setOrigin(16 / 2, 44 / 2);
+	sprite.setPosition(256 / 2, 250 * .75);
 }
 
-void GlassJoe::update()
+void GlassJoe::update(sf::Event& window)
 {
 
 }
-void GlassJoe::draw()
+void GlassJoe::draw(sf::RenderWindow& window)
 {
-
+	window.draw(sprite);
 }
 
 int GlassJoe::getHealth()
@@ -27,8 +33,8 @@ int GlassJoe::getDamage()
 	return 11;
 }
 
-void GlassJoe::wasHit(Player& p)
-{
-	health -= 6;//this will need to be changed later, just for testing
-}
+//void GlassJoe::wasHit(Player& p)
+//{
+	//health -= 6;//this will need to be changed later, just for testing
+//}
 
