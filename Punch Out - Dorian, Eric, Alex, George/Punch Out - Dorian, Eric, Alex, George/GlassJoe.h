9d269c8c225db535702f0afa58a4a510f4566f5c
EricSchultz05 <125312833+EricSchultz05@users.noreply.github.com>
2023-02-21 15:59:15 -0600

    Add files via upload

--- Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/GlassJoe.h
+++ Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/GlassJoe.h
@@ -1,12 +1,14 @@
 #pragma once
 #include "Opponent.h"
+#include <SFML/Graphics.hpp>
+
 class GlassJoe : public Opponent
 {
 public:
 	GlassJoe();
 	~GlassJoe();
-	void update();
-	void draw();
+	void update(sf::Event&);
+	void draw(sf::RenderWindow&);
 	int getHealth();
 	int getDamage();
 	void wasHit(Player&);
@@ -14,5 +16,7 @@ private:
 	int health, stamina, canDodgeCD, cantDodgeCD, rank, age, weight;
 	bool isInPattern, isStunned;
 	std::string name, location, stats;
+	sf::Sprite sprite;
+	sf::Texture texture;
 };
 
