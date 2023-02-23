2f0910b1f9f29b0abaf0048a1868879c14f90782
RanbowMellon <109363895+RanbowMellon@users.noreply.github.com>
2023-02-23 15:35:10 -0600

    stage doable for uimanaging

--- Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/Game.cpp
+++ Punch Out - Dorian, Eric, Alex, George/Punch Out - Dorian, Eric, Alex, George/Game.cpp
@@ -7,7 +7,7 @@ Game::Game()
 	oppoKO = 0;
 	points = 0;
 	round = 1;
-	state = 1;
+	state = 0;
 	time = 0;
 
 	mainTheme.openFromFile("sounds/Bout Theme.wav");
@@ -20,25 +20,14 @@ void Game::play(sf::RenderWindow& window, sf::Event& event, sf::View& view)
 
 	switch (state) //what ever the current state of the game, game does this. Ex, if we're not in the fight state don't show or update fight screen
 	{	
-		case 0:
-			view.setCenter(512 * 0.75, 480 /4); //right of the fight screen, 3/4 of main's screens x, 1/4 of main screens Y
+		case 0: //First ring
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num1) && !isKeyPressed)
 			{
 				state = 1;
 				isKeyPressed = true;
-				mainTheme.play();
-			}
-			
-			break;
-	
-		case 1: //Fighting state, little mac vs the opponent
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num1) && !isKeyPressed)
-			{
-				state = 0;
-				isKeyPressed = true;
 				mainTheme.stop();
 			}
-			view.setCenter(512 / 4, 480 / 4); // 1/4 of the main screen in both axis's
+			view.setCenter(128 + 2, 110 + 2); //center of first + 2 for border
 			//update FightUI, player, Opponent
 			littleMax.updatePlayer(event);
 
@@ -46,6 +35,19 @@ void Game::play(sf::RenderWindow& window, sf::Event& event, sf::View& view)
 			littleMax.drawPlayer(window);
 			
 			break;
+		
+		case 1: //Second ring
+			view.setCenter(384 + 3, 110 + 2); //center of second + 3 for border
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num1) && !isKeyPressed)
+			{
+				state = 0;
+				isKeyPressed = true;
+				mainTheme.play();
+			}
+			
+			break;
+	
+		
 
 		/*
 			using a bigger comment for  stuff Im gonna add in the future,
