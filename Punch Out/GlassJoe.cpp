c1d7ffb179e4862ab2d8df66b4573159a1c6376d
OddDman123 <125312692+OddDman123@users.noreply.github.com>
2023-02-28 11:11:40 -0600

    Made Glass joe actually work
    Gave him a derefrence, and changed his funcs to match opponent

--- Punch Out/GlassJoe.cpp
+++ Punch Out/GlassJoe.cpp
@@ -6,17 +6,23 @@ GlassJoe::GlassJoe()
 	name = "Glass Joe";
 	location = "Paris, France";
 	stats = "1-99      1 KO";
-	texture.loadFromFile("sprites/littleMac.png");
+	texture.loadFromFile("punchout sprites/GlassJoe.png");
 	sprite.setTexture(texture);
-	sprite.setTextureRect(sf::IntRect(6, 0, 16, 44));
-	sprite.scale(1.5, 1.5);
-	sprite.setOrigin(16 / 2, 44 / 2);
-	sprite.setPosition(256 / 2, 250 * .75);
+	sprite.setTextureRect(sf::IntRect(0, 0, 55, 115));
+	//sprite.scale(1.5, 1.5);
+	sprite.setOrigin(55 / 2, 115 / 2);
+	sprite.setPosition(256 / 2, 100);
 }
 
-void GlassJoe::update(sf::Event& window)
+void GlassJoe::update(int time)
 {
-
+	if (time < 4000)
+	{
+		if (time % 400 == 0)
+			sprite.setTextureRect(sf::IntRect(0, 115 * 3, 50, 115));
+		else
+			sprite.setTextureRect(sf::IntRect(50, 115 * 3, 50, 115));
+	}
 }
 void GlassJoe::draw(sf::RenderWindow& window)
 {
@@ -38,3 +44,5 @@ int GlassJoe::getDamage()
 	//health -= 6;//this will need to be changed later, just for testing
 //}
 
+GlassJoe::~GlassJoe()
+{}
