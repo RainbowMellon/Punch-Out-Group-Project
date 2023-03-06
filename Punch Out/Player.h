66c464458507dfb45ee9672354ae7399dcedc99f
AlexElletson1 <125312839+AlexElletson1@users.noreply.github.com>
2023-03-06 14:31:31 -0600

    Update Player.h

--- Punch Out/Player.h
+++ Punch Out/Player.h
@@ -16,13 +16,14 @@ public:
 	int getDamage();
 	int getStarCount();
 	bool isPunching();
+	bool isBlocking();
 	//void wasHit(Opponent);  <--somethings broken
 
 private:
 	sf::Sprite sprite;
 	sf::Texture texture;
 	int moveCoolD, dir, startCount, damage, health;
-	bool isWinded, keyPressed, punching;
+	bool isWinded, keyPressed, punching, blocking, dodging, upper;
 	sf::SoundBuffer buffer;
 	sf::Sound sound;
 };
