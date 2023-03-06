ebeefe8a67109ec2a46c6ac000848d6c6ab79e61
EricSchultz05 <125312833+EricSchultz05@users.noreply.github.com>
2023-03-06 13:41:05 -0600

    Added "functioning" Glass Joe

--- Punch Out/GlassJoe.h
+++ Punch Out/GlassJoe.h
@@ -7,11 +7,16 @@ class GlassJoe : public Opponent
 public:
 	GlassJoe();
 	~GlassJoe();
-	void update(int time);
+	void update(int time, Game&);
 	void draw(sf::RenderWindow&);
 	int getHealth();
 	int getDamage();
 	void wasHit(Player&);
+	void vive(int);
+	void jab(int);
+	void hook(int);
+	void idle(int);
+	
 
 private:
 	int health, stamina, canDodgeCD, cantDodgeCD, rank, age, weight;
