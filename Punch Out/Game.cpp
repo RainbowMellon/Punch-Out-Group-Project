3b621edd454a467d7892e0a5d01c38c1c98b6cee
OddDman123 <125312692+OddDman123@users.noreply.github.com>
2023-03-14 15:41:30 -0500

    updated game state documentation

--- Punch Out/Game.cpp
+++ Punch Out/Game.cpp
@@ -21,7 +21,7 @@ Game::Game(sf::RenderWindow& window, sf::View& view)
 
 void Game::play(sf::RenderWindow& window, sf::Event& event, sf::View& view)
 {
-	if (event.type == sf::Event::KeyReleased) // No repeat keys, because window.setKeyRepeat doesn't work in this scenario
+		if (event.type == sf::Event::KeyReleased) // No repeat keys, because window.setKeyRepeat doesn't work in this scenario
 		isKeyPressed = false;
 
 	switch (state) //what ever the current state of the game, game does this. Ex, if we're not in the fight state don't show or update fight screen
@@ -30,8 +30,8 @@ void Game::play(sf::RenderWindow& window, sf::Event& event, sf::View& view)
 			view.setCenter(898 + 5, 110 + 2); //center of first + borders
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
 			{
-				//do transitions
-				state = 3;
+				view.setCenter(128 + 2, 110 + 2);
+				state = 1;
 			}
 			break;
 		case 1: //Stats screen
@@ -39,13 +39,34 @@ void Game::play(sf::RenderWindow& window, sf::Event& event, sf::View& view)
 			//draw states, if round >= 2, draw the opponents and docs quotes
 
 			//when enter is press, scroll down to round image, fade in black box, then switch states
-
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter))
+			{
+				state = 2;
+			}
 			break;
 
 		case 2: //view transistion? I might make stats screen absorb this
+			window.draw(fadeout);
+
+			if (view.getCenter().y < 110 * 3)
+				view.move(0, 5);
+			else if (time < 75)
+				time++;
+			else if (fadeout.getFillColor().a < 255)
+				fadeout.setFillColor(sf::Color(0, 0, 0, fadeout.getFillColor().a + 5));
+			else //if (mainTheme.openFromFile("sounds/Bout Theme.wav"))
+			{
+				time = 0;
+				fadeout.setFillColor(sf::Color(0, 0, 0, 0));
+				//backgroundTexture.loadFromFile("punchout sprites/stage1.png");
+				mainTheme.play();
+				state = 3;
+			}
 
 			break;
-		case 3: //Fight screen
+		case 3: // intro to the fight
+			break;
+		case 4: //Fight screen
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num1) && !isKeyPressed)
 			{
 				state = 4;
@@ -71,36 +92,33 @@ void Game::play(sf::RenderWindow& window, sf::Event& event, sf::View& view)
 			//Draw Player, Opponent, FightUI
 			opponent->draw(window);
 			littleMac.drawPlayer(window);
-			UI.drawStats(littleMac, *opponent, time, 1, round);
+			UI.drawStats(littleMac, *opponent, time, 1);
 			std::cout << time << std::endl;
 
 			//If the opponent or player is KOed, go to win or lose screen
 			break;
 		
-		case 4: //Second ring
+		case 5: //Mario counts for opponent
 			view.setCenter(384 + 3, 110 + 2); //center of second + 3 for border
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num1) && !isKeyPressed)
 			{
 				state = 3;
-				round++;
 				isKeyPressed = true;
 				mainTheme.play();
 			}
-			UI.drawStats(littleMac, *opponent, time, 2, round);
+			UI.drawStats(littleMac, *opponent, time, 2);
 			break;
-	
-		
+		case 6: //Mario counts for little mac
+			break;
+		case 7: // fadeout into win screen
+			break;
+		case 8: //win screen, will either fadeout to state 1 (while changing opponent), or scroll to state 9
+			break;
+		case 9: // scroll to title bout screen, then change state to 1(while changing oppponent
+			break;
+		case 10: //Doc biking with mac, after a title bout
+				break;
 
-		/*
-			using a bigger comment for  stuff Im gonna add in the future,
-			
-			states:
-			player & opponent stats states;
-			a state that will just croll the view then go to the next state
-			
-			im tired and am going to eat
-		
-		*/
 	}
 
 }
