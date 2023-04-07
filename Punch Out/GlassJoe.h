a273aadacc58c4eefa34cbc1ced93d9be2801bf6
OddDman123 <125312692+OddDman123@users.noreply.github.com>
2023-04-07 14:42:42 -0500

    Update GlassJoe.h

--- Punch Out/GlassJoe.h
+++ Punch Out/GlassJoe.h
@@ -18,6 +18,13 @@ public:
 	void hook(int);
 	void idle(int);
 	
+	bool hasIntro();
+	sf::String introMusicFile();
+
+	//Returns true while the character is doing his intro, returns false when it's done
+	bool Intro();
+	bool toStage();
+	
 
 private:
 	int health, stamina, canDodgeCD, cantDodgeCD, rank, age, weight,jabsHit,upsHit;
